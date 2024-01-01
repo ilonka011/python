1cc2ab8a69787bf653786716b1292f7050bf25a8
Elona Gorter <111061455+ilonka011@users.noreply.github.com>
2024-01-01 16:30:57 -0500

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+adj= input('Adjective: ')
+adj2 = input('Another adjective: ')
+verb = input("Verb: ")
+
+madlib = f'Learning every day is so {adj}! You are able to sense how your brain is {verb} its energy. Our life is very {adj2} and we have to always be thankful for all the opportunities. '
+print(madlib)
+
+name = input('Your name: ')
+age = input('Your age: ')
+residence = input('Where do you live?(city) ')
+goal = input('One of your goals for this year: ')
+
+your_story = f'My name is {name} and I am {age} years old. Currently, I live in {residence}. Today is the first day of 2024 and one of my resolutions for this year is {goal}. I will work hard towards my goal. '
+print(your_story)
