88527c0f0c6253338f3549f49e1edc5e310d31f8
Elona Gorter <111061455+ilonka011@users.noreply.github.com>
2024-01-02 11:19:32 -0500

    Add files via upload

---
+++ commands.py
@@ -0,0 +1,25 @@
+# checking the 'type' command
+print(type(72))
+print(type('Leondardo da Vinci'))
+print(type([223, 14, 23, 20, 99999]))
+
+# checking the 'range' command
+for i in range(1, 7):
+    print(i)
+
+# checking the 'round'' command
+print(round(23.76890013))
+print(round(126.1112))
+
+# cheking the 'input' command
+input('What is the weather like? ')
+
+#checking the 'len' command
+love = 'I love him'
+print(len(love))
+
+list1 = [11, 23, 99, 20, 97, 87]
+print(len(list1))
+
+never_mind = 'Sorry for saying that'
+print(len(never_mind))
