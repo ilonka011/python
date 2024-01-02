376982c2d8b2a958e2014ffa08f54c6f055b2912
Elona Gorter <111061455+ilonka011@users.noreply.github.com>
2024-01-02 16:27:48 -0500

    Add files via upload

---
+++ mini.py
@@ -0,0 +1,26 @@
+languages = ['Ukrainian', 'Chinese', 'Arabic', 'English']
+for language in languages: 
+    print(language)
+
+str = '777Vitayoo'
+print(str.isalnum())
+str = 'def65!!!?'
+print(str.isalnum())
+
+letter = 'giving up on you is never an option'
+print(letter.capitalize())
+
+nice_message = 'Good morning, darling! How are you doing today? How did you sleep? I hope you did sleep well. I miss you, honey.'
+
+saying = 'I wanted to take a shower and workout'
+print(saying.find('shower'))
+print(saying.find('w'))
+print(saying.find('4'))
+
+my_name = 'Ilonchik is here'
+print(my_name.center(20))
+
+fruit = ['orange', 'apple', 'banana', 'strawberry', 'blueberry', 'persimmon']
+fruit.append('kiwi')
+fruit.insert(3, 'lemon')
+print(fruit)
