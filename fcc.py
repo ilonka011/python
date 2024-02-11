12cf67303e4a547cb752665268cde0639838fa7d
Elona Gorter <111061455+ilonka011@users.noreply.github.com>
2024-02-11 17:40:07 -0500

    Update fcc.py

--- fcc.py
+++ fcc.py
@@ -9,3 +9,8 @@ del users['Jane']
 # Testing the append; may have errors
 users.append['Sofia', 14]
 print(users)
+
+def main():
+    numbers = [11, 14, 22, 23, 75, 68, 25, 45]
+    for n im range(numbers):
+        print(n)
